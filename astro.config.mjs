f225b13edcc6bf088c9742dc4f12585f7d12fe1a
mustak <mhyaqub@yahoo.com>
2024-04-17 18:50:38 +0100

    Added two new pages - about and blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,16 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 const isProduction = import.meta.env.PROD;
-const base = 'astro-test';
-const site = 'https://mustak.github.com';
+const prodBase = 'astro-test';
+const prodSite = 'https://mustak.github.com';
 
 const prodconfig = {
-    site,
-    base,
+    site: prodSite,
+    base: prodBase,
 };
 
 const config = isProduction ? prodconfig : {};
+
 export default defineConfig(config);
+
 // https://github.com/mustak/astro-test
