ab38d4de2626d16a97b1e373a3547ac83dd8d838
mustak <mhyaqub@yahoo.com>
2024-04-17 19:08:25 +0100

    update 4

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 const isProduction = import.meta.env.PROD;
-const prodBase = 'astro-test';
+const prodBase = '/astro-test';
 const prodSite = 'https://mustak.github.com';
 
 const prodconfig = {
