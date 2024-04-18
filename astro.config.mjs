42b2237d6380650c23439f45463201915cc7eba5
mustak <mhyaqub@yahoo.com>
2024-04-18 19:05:07 +0100

    setup react, tailwind and msx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,32 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
+import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
+import partytown from '@astrojs/partytown';
+import mdx from '@astrojs/mdx';
+
 const isProduction = import.meta.env.PROD;
 const prodBase = '/astro-test';
 const prodSite = 'https://mustak.github.com';
 
+const baseConfig = {
+    integrations: [react(), tailwind(), partytown(), mdx()],
+};
 const prodconfig = {
+    ...baseConfig,
     site: prodSite,
     base: prodBase,
     trailingSlash: 'always',
 };
-
 const devconfig = {
+    ...baseConfig,
     // site: 'http://localhost:4321',
     // base: '/',
     // trailingSlash: 'always',
 };
-
 const config = isProduction ? prodconfig : devconfig;
 
+// https://astro.build/config
 export default defineConfig(config);
 
 // https://github.com/mustak/astro-test
